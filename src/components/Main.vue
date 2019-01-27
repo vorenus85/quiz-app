62406751bae1574cc63c9563e1903df24353ff15
Janos Perge <janos.perge@gmail.com>
2019-01-27 23:05:35 +0100

    add settings board

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,6 +12,40 @@
               :key="category.id">
               {{ category.name }}
             </button>
+            <h3 class="mt-3 mb-3">Quiz settings</h3>
+            <div class="settings-board">
+              <div class="form-group d-flex align-items-center">
+                <label for="questionPerCategory" style="width: 250px" class="text-left">Possible question per category:</label>
+                <input
+                  id="questionPerCategory"
+                  :type="number"
+                  :min="10"
+                  :max="20"
+                  class="form-control ml-3"
+                  style="width: 70px"
+                  v-model="questionPerCategory"
+                  :value="questionPerCategory"
+                  readonly>
+                <button class="btn btn-info ml-2" @click="questionPerCategoryInc()">+</button>
+                <button class="btn btn-info ml-2" @click="questionPerCategoryDec()">-</button>
+              </div>
+              <div class="form-group d-flex align-items-center">
+                <label for="questionPerCategory" style="width: 250px" class="text-left">Round per category:</label>
+                <input
+                  id="questionForPlay"
+                  :type="number"
+                  :min="5"
+                  :max="10"
+                  class="form-control ml-3"
+                  style="width: 70px"
+                  v-model="questionForPlay"
+                  :value="questionForPlay"
+                  readonly>
+                <button class="btn btn-info ml-2" @click="questionForPlayInc()">+</button>
+                <button class="btn btn-info ml-2" @click="questionForPlayDec()">-</button>
+              </div>
+            </div>
+
           </div>
           <div id="questions" v-else>
             <h2>Choosen category: {{selectedCategoryName}}</h2>
@@ -112,7 +146,11 @@ export default {
         }
       ],
       questionPerCategory: 10,
+      questionPerCategoryMax: 20,
+      questionPerCategoryMin: 10,
       questionForPlay: 5,
+      questionForPlayMax: 10,
+      questionForPlayMin: 5,
       actQuestion: '',
       possAnswers: [],
       userAnswers: [],
@@ -123,6 +161,26 @@ export default {
     }
   },
   methods: {
+    questionPerCategoryInc: function () {
+      if (this.questionPerCategory <= this.questionPerCategoryMax - 1 && this.questionPerCategory >= this.questionPerCategoryMin - 1) {
+        this.questionPerCategory++
+      }
+    },
+    questionPerCategoryDec: function () {
+      if (this.questionPerCategory - 1 <= this.questionPerCategoryMax && this.questionPerCategory - 1 >= this.questionPerCategoryMin) {
+        this.questionPerCategory--
+      }
+    },
+    questionForPlayInc: function () {
+      if (this.questionForPlay <= this.questionForPlayMax - 1 && this.questionForPlay >= this.questionForPlayMin - 1) {
+        this.questionForPlay++
+      }
+    },
+    questionForPlayDec: function () {
+      if (this.questionForPlay - 1 <= this.questionForPlayMax && this.questionForPlay - 1 >= this.questionForPlayMin) {
+        this.questionForPlay--
+      }
+    },
     getCategory: function (id, name) {
       this.selectedCategoryName = name
       this.selectedCategoryId = id
@@ -230,6 +288,11 @@ export default {
       background: $red!important;
       color: $white;
     }
+  }
+
+  .settings-board {
+    width: 400px;
+    margin: 0 auto;
   }
 
   .question {
